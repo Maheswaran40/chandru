10b32d9f969e2fea4c35fade235de4c2393e2d45
Maheswaran40 <eswermahes@gmail.com>
2026-08-17 12:38:01 +0530

    server with fs module

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,38 @@
 
-// const http = require("http")  // http 
+const http = require("http")
+const fs = require("fs")
 
-//  createServer is used for server creation
-// user sends request to server , server sends responce to user
-// const server = http.createServer((req,res)=>{ 
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
 
-    // status code for how the responce will send
-        // res.statusCode = 200
-        //  message sends from the server through  "end"  keyword
-        // res.end(`<h1>Hwllo world</h1>`)
-       
-        
-// })
+        fs.readFile("./pages/home.html", "utf-8", (err, data) => {
+            if (err) throw err;
+            res.statusCode = 200
+            res.end(data)
+        })
 
-//set port number for server listening
-// server.listen(8080 , () =>  console.log("server is running on the port http://localhost:8080"))
+    }
+    else if (req.url == "/about") {
 
+        fs.readFile("./pages/about.html", "utf-8", (err, data) => {
+            if (err) throw err;
+            res.statusCode = 200
+            res.end(data)
+        })
 
-
-
-const http =require("http")
-
-
-const server  =  http.createServer((req,res)=>{
-   if( req.url == "/"){
-    res.statusCode = 200
-    res.end("Welcome to home page")
-   }
-   else if(req.url == "/about"){
-    res.statusCode =200 
-    res.end ("welcome to about page")
-   }
-   else if(req.url == "/contact"){
-    res.statusCode =200 
-    res.end ("welcome to contact page")
-   }
-   else {
-    res.statusCode = 404
-    res.end ("page not found")
-   }
+    }
+    else if (req.url == "/contact") {
+        fs.readFile("./pages/contact.html", "utf-8", (err, data) => {
+            if (err) throw err;
+            res.statusCode = 200
+            res.end(data)
+        })
+    }
+    else {
+        res.statusCode = 404
+        res.end("page not found")
+    }
 })
 
 
-server.listen(3000 , ()=> console.log("server is running http://localhost:3000"))
+server.listen(3000, () => console.log("server is running http://localhost:3000"))
